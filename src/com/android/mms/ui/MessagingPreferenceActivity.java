6b02c66e25bb253f15cb1931958f229c9484c96d
travp624 <stygianstudiosdevs@gmail.com>
2012-05-02 23:01:18 -0500

    remove quick emoji settings

--- src/com/android/mms/ui/MessagingPreferenceActivity.java
+++ src/com/android/mms/ui/MessagingPreferenceActivity.java
@@ -74,7 +74,6 @@ public class MessagingPreferenceActivity extends PreferenceActivity
     public static final String STRIP_UNICODE            = "pref_key_strip_unicode";
     public static final String ENABLE_EMOJIS            = "pref_key_enable_emojis";
     public static final String FULL_TIMESTAMP           = "pref_key_mms_full_timestamp";
-    public static final String ENABLE_QUICK_EMOJIS      = "pref_key_emojis_quick";
     public static final String SENT_TIMESTAMP           = "pref_key_mms_use_sent_timestamp";
     public static final String NOTIFICATION_VIBRATE_PATTERN = "pref_key_mms_notification_vibrate_pattern";
     public static final String NOTIFICATION_VIBRATE_PATTERN_CUSTOM = "pref_key_mms_notification_vibrate_pattern_custom";

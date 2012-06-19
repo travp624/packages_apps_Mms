e1aa8231851dbccd7f527ff530925ef62a87b774
Phil Sweeney <cyanogenmod@sweeneymail.com>
2012-06-19 09:47:39 +0400

    Revert "Remove message notification while in conversation ui"
    This change causes the MMS app to hang repeatedly when moving in/out of long conversations on my phone with a large database of messages.  Prior to this change there are zero issues.
    
    This reverts commit 6fb86a67f74dd727407ccf9a2eb758aff571d7f0

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -3871,7 +3871,6 @@ public class ComposeMessageActivity extends Activity
                     mDataSetChangedListener = new MessageListAdapter.OnDataSetChangedListener() {
         public void onDataSetChanged(MessageListAdapter adapter) {
             mPossiblePendingNotification = true;
-            checkPendingNotification();
         }
 
         public void onContentChanged(MessageListAdapter adapter) {
